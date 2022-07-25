8a57a118e806cea4405bec018af1a13e589c6f2a
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-25 13:48:54 +0900

    WebView 완성

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0725_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0725_2/MainActivity.java
@@ -3,12 +3,48 @@ package kr.hs.emirim.s2125.mirim_project_0725_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editUrl;
+    WebView web;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editUrl = findViewById(R.id.edit_url);
+        web = findViewById(R.id.web);
+        Button btnGo= findViewById(R.id.btn_go);
+        Button btnRreV= findViewById(R.id.btn_prev);
+
+        web.setWebViewClient(new WebClient());
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.loadUrl(editUrl.getText().toString());
+            }
+        });
+        btnRreV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                web.goBack();
+            }
+        });
+    }
+    class WebClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
     }
 }
